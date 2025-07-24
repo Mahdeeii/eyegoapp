e40fcd1e9ce6b8982d828eed429f771ba80ecc15
Mahdeei <mohamedmahdeei6@gmail.com>
2025-07-24 15:11:33 +0300

    feat: added frontend HTML page for LoadBalancer response

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route
 app.get('/', (req, res) => {
-  res.send('Hello Eyego');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
